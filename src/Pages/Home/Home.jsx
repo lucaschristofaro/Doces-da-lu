238bd22cdb1f520e93f693e6ae923d78da930784
lucaschristofaro <lucassilva8456@gmail.com>
2022-12-18 18:17:08 -0300

    alteração da home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { BsWhatsapp } from "react-icons/bs";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import banner from "../../assets/doces/banner1.jpg";
 
 const Home = () => {
 	AOS.init();
@@ -9,14 +8,14 @@ const Home = () => {
 		<div className="border-t-4 border-rose-400 ">
 			<div data-aos="flip-up" data-aos-duration="1500">
 				<section className="relative bg-[url(https://images.unsplash.com/photo-1574085733277-851d9d856a3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80)] bg-cover bg-center bg-no-repeat">
-					<div className="absolute inset-0 bg-white/40 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/30 sm:to-white/25"></div>
+					<div className="absolute inset-0 "></div>
 
 					<div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
 						<div className="max-w-xl text-center sm:text-left">
 							<div data-aos="fade-down" data-aos-duration="1000">
-								<h1 className="text-3xl font-extrabold font-Paytone  sm:text-5xl">
+								<h1 className="text-3xl font-extrabold font-Paytone stroke-blue-500 text-[#DAD1B9]  sm:text-5xl">
 									Doces da Lu
-									<strong className="block text-4xl font-extrabold text-rose-700">
+									<strong className="block text-4xl font-extrabold stroke-black text-[#94FFEB]">
 										A felicidade mora num pedaço de bolo{" "}
 									</strong>
 								</h1>
